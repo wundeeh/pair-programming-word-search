1770a3f798ed93aea034119bc634c5a31820dbf1
wndy <114026479+wundeeh@users.noreply.github.com>
2022-11-23 19:09:51 -0600

    Update wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,67 @@
-const wordSearch = (letters, word) => { 
+const transpose = function (matrix) {
+    // Put your solution here
+    let newMatrix = [];
+    for (let arr in matrix[0]) {
+      let rowArray = [];
+      for (let row in matrix) {
+        rowArray.push(matrix[row][arr]);
+        //console.log("rowArray:", rowArray);
+      }
+      newMatrix.push(rowArray);
+    //   console.log("newMatrix:", newMatrix)
+    }
+   return newMatrix;
+  };
+
+  const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) {
+            return true;
+        } else {
+            let vertLetters = transpose(letters);
+            vertLetters = vertLetters.map(ls => ls.join(''))
+            for (lr of vertLetters) {
+                if (lr.includes(word)) {
+                    return true;
+                }
+            }
+        }
     }
+    return false;
 }
 
-module.exports = wordSearch
+
+
+console.log(wordSearch([
+    ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+    ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+    ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+    ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  ], 'FRANK')
+  );
+
+console.log("");
+console.log("-------------------------------------------------------");
+console.log("");
+
+console.log(wordSearch([
+    ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+    ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+    ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+    ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  ], 'SEINFELD')
+);
+
+
+module.exports = wordSearch;
